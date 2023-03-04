569db11aa8e88013df7b0f2cfd0075093af52873
Joey Groves <joeygroves@joeys-mbp.home>
2023-03-04 02:56:54 +0000

    Added text for quick access to a list of cities weather

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 
 function TopButtons() {
-    return (
-        <div>TopButtons</div>
-    )
+
+    const cities = [
+        {
+            id: 1,
+            title: 'London'
+        },
+
+        {
+            id: 2,
+            title: 'Tokyo'
+        },
+
+        {
+            id: 3,
+            title: 'Pamplona'
+        },
+
+        {
+            id: 4,
+            title: 'Berlin'
+        },
+
+        {
+            id: 5,
+            title: 'Zurich'
+        },
+
+        {
+            id: 6,
+            title: 'Vancouver'
+        },
+
+        {
+            id: 7,
+            title: 'Portland'
+        }
+    ]
+
+    return <div className="flex items-center justify-center my-6">
+        {cities.map((city) => (
+            <button className="text-white text-lg font-medium">{city.title}</button>
+        ))}
+    </div>;
 }
 
-export default TopButtons
+export default TopButtons;
