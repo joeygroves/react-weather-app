5fd50fd552f0ca9fdb59dd4daaa77899174fb1be
Joey Groves <joeygroves@joeys-mbp.home>
2023-03-04 15:59:58 +0000

    Added search bar click and key press event handlers and location search

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,12 +1,42 @@
 import React from 'react'
 import { UilSearch, UilMapMarker } from '@iconscout/react-unicons'
+import { useState } from 'react';
 
 
-function Inputs() {
+function Inputs({ setQuery, units, setUnits }) {
+    const [city, setCity] = useState("");
+
+    const handleSearchClick = () => {
+        if (city !== '') setQuery({q: city})
+    }
+    
+    const handleSearchEnter = (event) => {
+        if (city !== ''&& event.key === "Enter") {
+            setQuery({q: city})
+        }
+    }
+
+    const handleLocationClick = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                let lat = position.coords.latitude;
+                let lon = position.coords.longitude
+
+                setQuery({
+                    lat,
+                    lon,
+                })
+            })
+        }
+    }
+
   return (
     <div className="flex flex-row justify-center my-6">
         <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
             <input 
+                value={city}
+                onChange={(e) => setCity(e.currentTarget.value)}
+                onKeyDown={handleSearchEnter}
                 type="text" 
                 placeholder="Search for a place"
                 className="text-xl font-light p-2 w-full shadow-xl focus:outline-non capitalize placeholder:capitalize"
@@ -14,10 +44,12 @@ function Inputs() {
             <UilSearch 
                 size={25} 
                 className="text-white cursor-pointer transition ease-out hover:scale-125" 
+                onClick={handleSearchClick}
             />
             <UilMapMarker 
                 size={25} 
                 className="text-white cursor-pointer transition ease-out hover:scale-125" 
+                onClick={handleLocationClick}
             />
         </div>
         <div className="flex flex-row w-1/4 items-center justify-center">
