583b2aebd9b640b141586a655e38d26d80a54097
Joey Groves <joeygroves@joeys-mbp.home>
2023-03-04 06:33:50 +0000

    Added buttons for celsius and farenheit options

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -11,10 +11,27 @@ function Inputs() {
                 placeholder="Search for a place"
                 className="text-xl font-light p-2 w-full shadow-xl focus:outline-non capitalize placeholder:capitalize"
             />
-
-            <UilSearch size={25} className="text-white cursor-pointer transitio ease-out hover:scale-125" />
-            <UilMapMarker size={25} className="text-white cursor-pointer transition ease-out hover:scale-125" />
-
+            <UilSearch 
+                size={25} 
+                className="text-white cursor-pointer transition ease-out hover:scale-125" 
+            />
+            <UilMapMarker 
+                size={25} 
+                className="text-white cursor-pointer transition ease-out hover:scale-125" 
+            />
+        </div>
+        <div className="flex flex-row w1/4 items-center justify-center">
+            <button 
+                name="metric" 
+                className="text-xl text-white font-light">
+                    °C
+            </button>
+            <p className="text-xl text-white mx-1">|</p>
+            <button 
+                name="imperial" 
+                className="text-xl text-white font-light">
+                    °F
+            </button>
         </div>
     </div>
     );
