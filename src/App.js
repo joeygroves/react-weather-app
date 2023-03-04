e50695b6dbb538807f7a9c51cb2ec12679f0d714
Joey Groves <joeygroves@joeys-mbp.home>
2023-03-04 09:35:22 +0000

    Creating and implementing WeatherDetails React component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
+import WeatherDetails from './components/WeatherDetails';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <TopButtons />
       <Inputs />
       <TimeAndLocation />
+      <WeatherDetails />
     </div>
   );
 }
