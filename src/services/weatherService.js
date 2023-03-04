41d532ae6b4f34057b9f9a4ba89ea7f2422e31c9
Joey Groves <joeygroves@joeys-mbp.home>
2023-03-04 11:20:29 +0000

    Implemented getWeatherData function

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,2 +1,11 @@
 const API_KEY = 'c4c4fad23b0100b1fd6b76616f52f1d1'
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/'
+
+/* API key from OpenWeatherMap */ 
+
+const getWeatherData = (infoType, searchParams) => {
+    const url = new URL(BASE_URL + '/' + infoType);
+    url.search = new URLSearchParams({...searchParams, appid:API_KEY})
+
+    return fetch(url).then((res) => res.json());
+}
