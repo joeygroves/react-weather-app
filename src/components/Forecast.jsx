06e3d239a989fe5ff0ba3808a9486780941a7600
Joey Groves <joeygroves@joeys-mbp.home>
2023-03-04 10:42:35 +0000

    Added 5 icons for 5 hour forecast

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Forecast() {
+function Forecast({ title }) {
   return (
     <div>
         <div className='flex items-center justify-start mt-6'>
             <p className='text-white font-medium uppercase'>
-                Hourly Forecast
+                {title}
             </p>
         </div>
         <hr className='my-2' />
@@ -13,7 +13,55 @@ function Forecast() {
         <div className='flex flex-row items-center justify-between text-white'>
             <div className='flex flex-col items-center justify-center'>
                 <p className='font-light text-sm'>
-                    16:30
+                    16:00
+                </p>
+                <img 
+                    src="http://openweathermap.org/img/wn/10d@2x.png" 
+                    alt=""
+                    className='w-12 my-1'
+                />
+                <p className='font-medium'>22°</p>
+            </div>
+
+            <div className='flex flex-col items-center justify-center'>
+                <p className='font-light text-sm'>
+                    17:00
+                </p>
+                <img 
+                    src="http://openweathermap.org/img/wn/10d@2x.png" 
+                    alt=""
+                    className='w-12 my-1'
+                />
+                <p className='font-medium'>22°</p>
+            </div>
+
+            <div className='flex flex-col items-center justify-center'>
+                <p className='font-light text-sm'>
+                    18:00
+                </p>
+                <img 
+                    src="http://openweathermap.org/img/wn/10d@2x.png" 
+                    alt=""
+                    className='w-12 my-1'
+                />
+                <p className='font-medium'>22°</p>
+            </div>
+
+            <div className='flex flex-col items-center justify-center'>
+                <p className='font-light text-sm'>
+                    19:00
+                </p>
+                <img 
+                    src="http://openweathermap.org/img/wn/10d@2x.png" 
+                    alt=""
+                    className='w-12 my-1'
+                />
+                <p className='font-medium'>22°</p>
+            </div>
+
+            <div className='flex flex-col items-center justify-center'>
+                <p className='font-light text-sm'>
+                    20:00
                 </p>
                 <img 
                     src="http://openweathermap.org/img/wn/10d@2x.png" 
